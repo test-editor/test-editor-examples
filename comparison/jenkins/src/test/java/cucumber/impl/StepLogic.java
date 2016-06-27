d468aaf87babfc10fe7b0dffd3f4845a7c1cb234
Franz Becker <franz.becker@signal-iduna.de>
2016-06-27 10:52:45 +0200

    improve Cucumber check if Jenkins is available

--- comparison/jenkins/src/test/java/cucumber/impl/StepLogic.java
+++ comparison/jenkins/src/test/java/cucumber/impl/StepLogic.java
@@ -5,17 +5,18 @@ import java.net.URL;
 
 public class StepLogic {
 
-    public boolean checkUrlAvailibility(String arg1) {
-        try {
-            HttpURLConnection connection = (HttpURLConnection) new URL(arg1).openConnection();
-            connection.setRequestMethod("HEAD");
-            int responseCode = connection.getResponseCode();
-            if (responseCode != 200) {
-                return false;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return true;
-    }
+	public boolean checkUrlAvailibility(String arg1) {
+		try {
+			HttpURLConnection connection = (HttpURLConnection) new URL(arg1).openConnection();
+			connection.setRequestMethod("HEAD");
+			int responseCode = connection.getResponseCode();
+			if (responseCode == 200) {
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
 }
